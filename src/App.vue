a8725fec3f12dbad98fc8ef2ddb02c209248ee00
Mihir Chaturvedi <plibither8@gmail.com>
2018-08-18 10:45:08 +0530

    No frills just finish the work

--- src/App.vue
+++ src/App.vue
@@ -29,61 +29,46 @@
 				.group.flex
 					.row.flex
 						input
-						.button.flex 🏆
 					.row.flex
 						input
-						.button.flex 🏆
 				.group.flex
 					.row.flex
 						input
-						.button.flex 🏆
 					.row.flex
 						input
-						.button.flex 🏆
 				.group.flex
 					.row.flex
 						input
-						.button.flex 🏆
 					.row.flex
 						input
-						.button.flex 🏆
 				.group.flex
 					.row.flex
 						input
-						.button.flex 🏆
 					.row.flex
 						input
-						.button.flex 🏆
 
 			.column.flex
 				.group.flex
 					.row.flex
 						input
-						.button.flex 🏆
 					.row.flex
 						input
-						.button.flex 🏆
 				.group.flex
 					.row.flex
 						input
-						.button.flex 🏆
 					.row.flex
 						input
-						.button.flex 🏆
 
 			.column.flex
 				.group.flex
 					.row.flex
 						input
-						.button.flex 🏆
 					.row.flex
 						input
-						.button.flex 🏆
 
 			.column.flex
 				.row.flex
 					input
-					.button
 
 </template>
 
@@ -96,59 +81,8 @@ export default {
 	components: {
 		heading
 	},
-	data () {
 
-		return {
-			teams: [
-				{
-					points: 0,
-					background: "linear-gradient(rgb(255, 12, 9) 0%, rgb(251, 104, 0) 100%)",
-					name: ""
-				}, {
-					points: 0,
-					background: "linear-gradient(rgba(13, 72, 161, 1) 0%, rgba(41, 181, 246, 1) 100%)",
-					name: ""
-				}, {
-					points: 0,
-					background: "linear-gradient(rgba(56, 142, 61, 1) 0%, rgba(139, 195, 74, 1) 100%)",
-					name: ""
-				}, {
-					points: 0,
-					background: "linear-gradient(rgba(255, 179, 0, 1) 0%, rgba(255, 238, 88, 1) 100%)",
-					name: ""
-				}, {
-					points: 0,
-					background: "linear-gradient(rgba(103, 58, 183, 1) 0%, rgba(102, 87, 128, 1) 100%)",
-					name: ""
-				}, {
-					points: 0,
-					background: "linear-gradient(rgba(0, 77, 64, 1) 0%, rgba(36, 138, 121, 1) 100%)",
-					name: ""
-				}
-			]
-		};
-
-	},
-	computed: {
-		highestPoints: function () {
-
-			let max = [
-				this.teams[0].points,
-				this.teams[1].points,
-				this.teams[2].points,
-				this.teams[3].points,
-				this.teams[4].points,
-				this.teams[5].points
-			].reduce(function(a, b) {
-
-				return Math.max(a, b);
-
-			});
-			return max === 0 ? 1 : max;
-
-		}
-	}
-};
+}
 
 </script>
 
